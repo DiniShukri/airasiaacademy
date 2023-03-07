3b9bd0e69d9c2de4ae5f7d36e3bc89b07b2cdb27
DiniShukri <127168634+DiniShukri@users.noreply.github.com>
2023-03-07 12:21:19 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -15,6 +15,13 @@ if option=='line chart':
     columns=['a', 'b', 'c'])
 
     st.line_chart(chart_data)
+    
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)    
 
 elif option=='T n C':
     st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
@@ -23,7 +30,7 @@ elif option=='T n C':
         st.write(pd.DataFrame({
         'Intplan': ['yes', 'yes', 'yes', 'no'],
         'Churn Status': [0, 0, 0, 1]
-
+        }))
 
 else:
     'Starting a long computation...'
@@ -38,5 +45,3 @@ else:
         time.sleep(0.1)
 
     '...and now we\'re done!'
-
-        }))
